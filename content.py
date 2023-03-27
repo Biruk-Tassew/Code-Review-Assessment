7685a8f94c2f2dca864dfe93275ba69f809d78f9
Hiwot Derese <Hiwotderese16@gmail.com>
2023-03-27 21:28:53 +0300

    add content file

--- content.py
+++ content.py
@@ -1,14 +1,5 @@
-# This is the count that will be increamented by increment_count function.
-count = 0
+def calculateArea(radius):
+    return 3.14 * radius ** 2
 
-# This function returns the product of the two numbers given
-def multiply(x, y):
-    return x * y
-
-# This function will increament the global variable count
-def increment_count():
-    global count
-    count += 1
-
-increment_count()
-print(count)
+def divide_numbers(a, b):
+    return a / b
