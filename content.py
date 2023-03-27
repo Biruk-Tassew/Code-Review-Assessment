5e4009d0b096ea4fac5b33deee059a5fff3de056
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-03-27 21:37:38 +0300

    Update content.py and added snippet_2

--- content.py
+++ content.py
@@ -12,3 +12,26 @@ def increment_count():
 
 increment_count()
 print(count)
+
+def is_valid_parenthesis(s):
+    dc = {
+        '(': ')',
+          "{": "}",
+         '[': ']'
+
+    }
+    stack = []
+    for i in s:
+        if i in dc.keys(): stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+            
+
+            op = stack.pop()
+            if dc[op] != i:
+                return False
+    if len(stack) != 0:
+        return False
+    
+    return True
