ff1f7ce4a416a6f89ee76dbee389339034aa55ff
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2023-03-27 18:43:13 +0300

    Update content.py according to the review given

--- content.py
+++ content.py
@@ -1,8 +1,11 @@
+# This is the count that will be increamented by increment_count function.
 count = 0
 
-def abc(x, y):
+# This function returns the product of the two numbers given
+def multiply(x, y):
     return x * y
 
+# This function will increament the global variable count
 def increment_count():
     global count
     count += 1
